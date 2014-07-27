9dd4f913192f064da377dd3e40849ff0fe6adf8a
Simon Brooke <simon@journeyman.cc>
2014-07-27 09:40:58 +0100

    Serious work on the buildall script (and project.clj) to capture a build signature in the MANIFEST.MF file.

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
 (defproject mw-parser "0.1.2-SNAPSHOT"
   :description "Parser for production rules for MicroWorld engine"
-  :url "http://example.com/FIXME"
+  :url "http://www.journeyman.cc/microworld"
+  :manifest {
+             ;; do not reformat this! It is important for the buildall script
+             ;; that each of these properties is on a line with nothing else.
+		"build-signature-version" "unset"
+		"build-signature-user" "unset"
+		"build-signature-email" "unset"
+		"build-signature-timestamp" "unset"
+             }
   :license {:name "GNU General Public License v2"
             :url "http://www.gnu.org/licenses/gpl-2.0.html"}
   :plugins [[lein-marginalia "0.7.1"]]
