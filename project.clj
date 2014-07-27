05458c37003fad95fa53627ad81c7684f87319de
Simon Brooke <simon@journeyman.cc>
2014-07-27 14:14:20 +0100

    Upversioned from 0.1.2 to 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject mw-parser "0.1.2"
+(defproject mw-parser "0.1.3-SNAPSHOT"
   :description "Parser for production rules for MicroWorld engine"
   :url "http://www.journeyman.cc/microworld"
   :manifest {
              ;; do not reformat this! It is important for the buildall script
              ;; that each of these properties is on a line with nothing else.
-		"build-signature-version" "0.1.2"
+		"build-signature-version" "0.1.3-SNAPSHOT"
 		"build-signature-user" "Simon Brooke"
 		"build-signature-email" "simon@journeyman.cc"
 		"build-signature-timestamp" "2014-07-27 11:55:13+01:00"
@@ -14,5 +14,5 @@
   :plugins [[lein-marginalia "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.trace "0.7.8"]
-                 [mw-engine "0.1.2"]
+                 [mw-engine "0.1.3-SNAPSHOT"]
                  ])
